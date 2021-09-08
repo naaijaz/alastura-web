010ea2f8ad0893f21e1553ff127084be8d78b473
naaijaz <aijazahmed7@gmail.com>
2021-09-08 11:52:37 +0400

    added cart to app.compunent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { ProductDataService } from './product-data.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  
-  
+
+
 })
 export class AppComponent {
   title = 'bootpro';
+  cart: any = [];
+
+  constructor(private productDataService: ProductDataService) {
+  }
+
+  showCart = true;
+
+  cardDisplay() {
+    this.showCart = !this.showCart;
+  }
 
 
+  ngOnInit(): void {
+    this.productDataService.CartObserver.subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
+  clearCart = () => {
+    this.productDataService.clearCart();
+  }
 }
